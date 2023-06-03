693be6da613afa5c8e6d5c8254d2fd7dc3f70abd
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-06-03 13:10:59 +0100

    update

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,18 +1,13 @@
-$(document).ready(function() {
-  const amenities = {};
 
-  $('input[type="checkbox"]').change(function() {
-    const amenityId = $(this).parent('li').data('id');
-    const amenityName = $(this).parent('li').data('name');
-
-    if ($(this).is(':checked')) {
-      amenities[amenityId] = amenityName;
-    } else {
-      delete amenities[amenityId];
-    }
-
-    const checkedAmenities = Object.values(amenities).join(', ');
-    $('.amenities-list').html('<li>' + checkedAmenities + '</li>');
-  });
-});
-
+// $(document).ready(function () {
+//   const amenities = {};
+//   $('input[type="checkbox"]').change(function () {
+//     if ($(this).is(':checked')) {
+//       amenities[$(this).data('id')] = $(this).data('name');
+//     } else {
+//       delete amenities[$(this).data('id')];
+//     }
+//     const checkedAmenities = Object.values(amenities).join(', ');
+//     $('.amenities-list').text(checkedAmenities);
+//   });
+// });
